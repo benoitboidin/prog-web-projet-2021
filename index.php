24b0466e93c4c40ea5cff10531629a97efa175f1
Antoine-gird <82816589+Antoine-gird@users.noreply.github.com>
2021-04-20 16:53:46 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
 	include("entete.inc.html");
-	
+	//require("connect.inc.php");
 	
 	echo "<h1>Accueil</h1>";
 	echo'<li><a href="connexion.php">Se connecter</a></li><br/>';
+/*	
+	try{
+		//Connexion
+		$c = new PDO("mysql:host=$host;dbname=$dbname", $login, $password);
+		$c->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo"<p>Connexion réussie.</p>\n";
 	
+		//ajouter les requêtes
 	
-	
+	} catch(PDOException $erreur) {
+		echo "<p>Erreur : ".$erreur->getMessage()."</p>\n";
+	}
+*/
 	
 	include("pied.inc.html");
 ?>
